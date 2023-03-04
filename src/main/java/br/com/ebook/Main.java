114f524c288a8bcb36dfda5c4f0c1cf1a22cdfbd
salomaotech <salomao@taimber.com>
2023-03-04 13:06:55 -0300

    Atualizando objetos (UPDATE)

--- src/main/java/br/com/ebook/Main.java
+++ src/main/java/br/com/ebook/Main.java
@@ -1,26 +1,21 @@
 package br.com.ebook;
 
-import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.EntityTransaction;
 
 public class Main {
 
     public static void main(String[] args) {
 
         EntityManager entityManager = ConectorJpa.getEntityManager();
-        Query query = entityManager.createQuery("select obj from Cliente obj");
-        List<Cliente> clienteList = query.getResultList();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
 
-        for (Cliente cliente : clienteList) {
+        Cliente cliente = entityManager.find(Cliente.class, 1L);
+        cliente.setNome("Jessica Dias");
+        cliente.setCpf("000.000.000.12");
 
-            System.out.println("ID: " + cliente.getId());
-            System.out.println("Nome: " + cliente.getNome());
-            System.out.println("CPF: " + cliente.getCpf());
-            System.out.println("Nascimento: " + cliente.getDataNascimento());
-
-        }
-
+        entityTransaction.commit();
         entityManager.close();
         ConectorJpa.fechar();
 
