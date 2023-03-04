c323c31ce660ec4644489e8d5c9962dfe68a5477
salomaotech <salomao@taimber.com>
2023-03-04 14:35:52 -0300

    Adicionado delete

--- src/main/java/br/com/ebook/Main.java
+++ src/main/java/br/com/ebook/Main.java
@@ -4,21 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 public class Main {
-
+    
     public static void main(String[] args) {
-
+        
         EntityManager entityManager = ConectorJpa.getEntityManager();
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
-
-        Cliente cliente = entityManager.find(Cliente.class, 1L);
-        cliente.setNome("Jessica Dias");
-        cliente.setCpf("000.000.000.12");
-
+        
+        Cliente cliente = entityManager.find(Cliente.class, 2L);
+        entityManager.remove(cliente);
+        
         entityTransaction.commit();
         entityManager.close();
         ConectorJpa.fechar();
-
+        
     }
-
+    
 }
